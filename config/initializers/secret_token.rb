ecdbc0b0ed03a0b05ba875f6d469057316027881
BruceP <brucepark0@gmail.com>
2014-02-10 12:47:17 -0800

    integrating fatfreecrm into rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrmApp::Application.config.secret_token = '07a3020bfab61b660eca19b6fca3ccb8faae9f97e9af3aec8fe68414e42bc24db4ddc69d80cccd4cbf94a05c3855c53f9c1e143c87be1ef8b4c6359543ab5063'
